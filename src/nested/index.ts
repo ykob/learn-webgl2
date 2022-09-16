562f7f0a7c2dac8c7836ae0b60338524553d5317
ykob <info@tplh.net>
2022-09-16 20:09:16 +0900

    added updateClearColor function.

--- src/nested/index.ts
+++ src/nested/index.ts
@@ -1,5 +1,6 @@
 const canvasWrap = document.querySelector<HTMLDivElement>("#canvas-wrap");
 const canvas = document.querySelector<HTMLCanvasElement>("#canvas-webgl");
+const gl = canvas!.getContext('webgl2');
 
 const resizeCanvas = () => {
   const width = canvasWrap!.clientWidth;
@@ -7,8 +8,22 @@ const resizeCanvas = () => {
   canvas!.height = (width / 3) * 2;
 };
 
+const updateClearColor = () => {
+  if (!gl) return
+  gl.clearColor(1, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.viewport(0, 0, 0, 0);
+}
+
+const update = () => {
+  updateClearColor();
+  requestAnimationFrame(update);
+}
+
 const init = () => {
   resizeCanvas();
+  update();
+
   window.addEventListener("resize", resizeCanvas);
 };
 
